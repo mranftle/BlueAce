2bf2446a5f5e4115e576a2ec2041918a8be91067
mranftle <matthew.ranftle@gmail.com>
2017-10-03 18:11:40 -0500

    user auth working

--- blue_ace_api/blue_ace_api/urls.py
+++ blue_ace_api/blue_ace_api/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
+from rest_framework_jwt.views import obtain_jwt_token
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^api-token-auth/', obtain_jwt_token)
 ]
