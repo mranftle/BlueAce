ab3bc463652bd80439f80e4addada51985700045
mranftle <matthew.ranftle@gmail.com>
2017-10-10 21:42:37 -0500

    models created

--- blue_ace_api/quickstart/models.py
+++ blue_ace_api/quickstart/models.py
@@ -1,21 +1,23 @@
 from django.db import models
-from django.modules.auth.models import User
+from django.contrib.auth.models import User
 class SportsGame(models.Model):
     id = models.AutoField(primary_key=True)
-    sport = models.CharField()
-    team1 = models.CharField()
-    team2 = models.CharField()
+    sport = models.CharField(max_length=20)
+    team1 = models.CharField(max_length=30)
+    team2 = models.CharField(max_length=30)
     score1 = models.IntegerField()
     score2 = models.IntegerField()
-    started = TimeClass()
+    started = models.TimeField()
 class Bet(models.Model):
     id = models.AutoField(primary_key=True)
-    user1 = models.ForeignKey(User, on_delete=models.CASCADE)
-    user2 = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user1 = models.ForeignKey(User, on_delete=models.CASCADE)
+    # user2 = models.ForeignKey(User, on_delete=models.CASCADE)
+    user1=models.IntegerField()
+    user2=models.IntegerField()
     game = models.ForeignKey(SportsGame, on_delete=models.CASCADE)
-    started = TimeClass()
+    started = models.TimeField()
 class Charity(models.Model):
-    id = model.AutoField(primary_key=True)
-    name = model.CharField()
-    description = model.TextField()
-    url = model.CharField()
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=30)
+    description = models.TextField()
+    url = models.CharField(max_length=50)
