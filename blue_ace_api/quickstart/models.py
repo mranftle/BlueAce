2c502fed625be4187a6d99967760b215987d42e0
QuintinFrerichsWUSTL <quintin.frerichs@gmail.com>
2017-10-10 21:31:14 -0500

    added models

--- blue_ace_api/quickstart/models.py
+++ blue_ace_api/quickstart/models.py
@@ -1,3 +1,21 @@
 from django.db import models
-
-# Create your models here.
+from django.modules.auth.models import User
+class SportsGame(models.Model):
+    id = models.AutoField(primary_key=True)
+    sport = models.CharField()
+    team1 = models.CharField()
+    team2 = models.CharField()
+    score1 = models.IntegerField()
+    score2 = models.IntegerField()
+    started = TimeClass()
+class Bet(models.Model):
+    id = models.AutoField(primary_key=True)
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE)
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(SportsGame, on_delete=models.CASCADE)
+    started = TimeClass()
+class Charity(models.Model):
+    id = model.AutoField(primary_key=True)
+    name = model.CharField()
+    description = model.TextField()
+    url = model.CharField()
