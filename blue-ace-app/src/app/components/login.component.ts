b2ae068cf10c167efa6045d0c9ca15e4f87cf4b8
mranftle <matthew.ranftle@gmail.com>
2017-11-16 22:49:37 -0600

    login and signup working

--- blue-ace-app/src/app/components/login.component.ts
+++ blue-ace-app/src/app/components/login.component.ts
@@ -4,13 +4,48 @@
 
 import {Component, OnInit, ViewChild } from '@angular/core'
 import { Router } from "@angular/router";
+import {AuthService} from "../services/auth.service";
 @Component({
   selector:'login',
   templateUrl: '../templates/login.component.html',
   styleUrls:['../stylesheets/login.component.css'],
 })
 
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
-  constructor(private router:Router){}
+  @ViewChild('username') username: any;
+  @ViewChild('password') password: any;
+
+  constructor(private router:Router,
+              private authService: AuthService){}
+
+  ngOnInit() {
+    this.authService.logout()
+  }
+
+  signup() {
+    let errMsg: string;
+    this.authService.signup(this.username.nativeElement.value, this.password.nativeElement.value)
+      .subscribe(
+        data => {
+          this.router.navigate((['/main']))
+        },
+        error => {
+          console.log('Login Error');
+        }
+      );
+  }
+
+  login() {
+    let errMsg: string;
+    this.authService.login(this.username.nativeElement.value, this.password.nativeElement.value)
+      .subscribe(
+        data => {
+          this.router.navigate((['/main']))
+        },
+        error => {
+          console.log('Login Error');
+        }
+      );
+  }
 }
