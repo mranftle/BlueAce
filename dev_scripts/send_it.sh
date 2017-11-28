d0bcaf3e9e747cc82f25a1071b6539d5a89ce165
mranftle <matthew.ranftle@gmail.com>
2017-11-27 19:46:49 -0600

    dev script changes

--- dev_scripts/send_it.sh
+++ dev_scripts/send_it.sh
@@ -1,4 +1,4 @@
 docker-compose up --build -d nginx-proxy
 docker-compose up --build -d letsencrypt-nginx
-docker-compose up --build -d pairs_api
-docker-compose up --build -d pairs_app
+docker-compose up --build -d blue_ace_api
+docker-compose up --build -d blue_ace_app
